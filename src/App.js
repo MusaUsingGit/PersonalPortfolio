f8592b6765a0fda4cb4a1e6c9c880efab8e61147
Musa Ahmed <154725338+MusaUsingGit@users.noreply.github.com>
2024-11-11 19:55:54 +0000

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { FaIconName } from "react-icons/fa";
+import { CiBowlNoodles } from "react-icons/ci";
+import { Navbar } from './Components/Navbar';
+
+
+function App() {
+  return (
+    <div className="bg-black">
+      <Navbar />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center h-24 w-max "></div>
+  )
+}
+
