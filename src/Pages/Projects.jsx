18e6af66cfb72ffe9b586a9f37ecc12ae2c88edb
Musa Ahmed <154725338+MusaUsingGit@users.noreply.github.com>
2024-11-13 08:40:23 +0000

    logo
    add logo fix title

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
+import React from "react";
+import Navbar from "../Components/Navbar";
 
 const Projects = () => {
   return (
     <div>
-      <Navbar /> 
-      <div className=" z-0 max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center text-white text-7xl font-extrabold bg-gradient-to-bl to-[#b000b5] from-[#b00b1E] bg-clip-text text-transparent">Projects</div>
+      <Navbar />
+      <div className=" z-0 max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center text-white text-7xl font-extrabold bg-gradient-to-bl to-[#b000b5] from-[#b00b1E] bg-clip-text text-transparent">
+        Projects
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
