31c908f07b5f5e0f6bc8126e195a0be446d0cb43
Musa Ahmed <154725338+MusaUsingGit@users.noreply.github.com>
2024-11-17 02:06:29 +0000

    Bonus

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,22 +1,36 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
-
+import me from "../Assets/Icon.png";
 const About = () => {
   return (
-    <div className="h-screen overflow-hidden"> 
-      <div className="sticky top-0 z-10">
-        <Navbar />
-      </div>
-      <div className="h-screen overflow-y-auto scroll-snap-y snap-mandatory no-scrollbar">
+    <div className="h-screen overflow-hidden scrollbar-hidden">
+      <Navbar className="sticky top-0 z-10" />
+      <div className="h-screen overflow-y-auto scroll-snap-y snap-mandatory no-scrollbar justify-center items-center">
         <div className="snap-center h-screen w-full flex items-center justify-center bg-gradient-to-bl from-[#b00b1E] to-[#b000b5] text-transparent bg-clip-text">
           <h1 className="text-7xl font-extrabold max-w-[800px] mx-auto text-center">
             Who am I?
           </h1>
         </div>
 
-        <div className="snap-center h-screen w-full flex items-center justify-center bg-gradient-to-bl from-[#b00b1E] to-[#b000b5] text-transparent bg-clip-text">
-          <h1 className="text-7xl font-extrabold max-w-[800px] mx-auto text-center">
-            I am a <strong className="animate-text-color-change">Student</strong>
+        <div className="flex items-center justify-center h-screen">
+          <div className="snap-center w-[80%] max-w-[1240px] flex flex-col items-center justify-center bg-gradient-to-bl from-[#b00b1E] to-[#b000b5] text-transparent bg-clip-text flex-wrap p-6 overflow-auto">
+            <h1 className="text-5xl md:text-7xl font-extrabold max-w-[800px] mx-[10%] text-center flex-1 whitespace-nowrap">
+              I am a{" "}
+              <strong>Student</strong>
+            </h1>
+            <img
+              src={me}
+              className="w-[50%] sm:w-[30%] md:w-[20%] flex-shrink h-auto mt-4 animate-pulse"
+            />
+            
+           
+            
+          </div>
+        </div>
+
+        <div className="snap-center h-full w-full flex items-center justify-normal bg-gradient-to-bl from-[#b00b1E] to-[#b000b5] text-transparent bg-clip-text">
+          <h1 className="text-5xl font-extrabold max-w-[800px] mx-auto text-Left">
+            Currently studying Computer Science BSc
           </h1>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*[.js,.jsx]"],
+  content: ["./src/**/*.{js,jsx}"], 
   theme: {
     extend: {
       animation: {
         "text-color-change": "textColorChange 3s infinite linear",
+        "image-hue-shift": "hueShift 15s infinite linear",
       },
       keyframes: {
         textColorChange: {
           "0%": { color: "#b00b1E" },
           "17%": { color: "#bada55" },
           "33%": { color: "#b000b5" },
-          "50%": { color: "#b060b5" },
+          "50%": { color: "#b00b5" },
           "66%": { color: "#FF6347" },
           "100%": { color: "#b00b1E" },
+        },
+        hueShift: {
+          "0%": { filter: "hue-rotate(0deg)" }, 
+          "100%": { filter: "hue-rotate(90deg)" }, 
         },
       },
     },
